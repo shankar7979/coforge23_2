b78232096f5f8df7f69f74cff3d10bdbb265f611
shankar7979 <shankar7979@gmail.com>
2023-09-29 17:39:29 +0530

    added

--- react/reactapp1/src/index.js
+++ react/reactapp1/src/index.js
@@ -7,6 +7,11 @@ import User from './user';
 import Hello1 from './Hello1';
 import Book1 from './book';
 import News from './News';
+import Person from './person/person';
+import Counter1 from './Counter1';
+import Address from './person/PersonInfo';
+import PersonInfo from './person/PersonInfo';
+import UserForm from './UserForm1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,7 +20,11 @@ root.render(
     {/* <User /> */}
     {/* <Hello1></Hello1> */}
     {/* <Book1></Book1> */}
-    <News langugae="hindi" article="election 2024"></News>
+    {/* <News langugae="hindi" article="election 2024"></News> */}
+    {/* <Counter1></Counter1> */}
+    {/* <Person></Person> */}
+    {/* <PersonInfo></PersonInfo> */}
+    <UserForm></UserForm>
   </React.StrictMode>
 );
 
