b0b6835309cd450de4711118fe996f7df5e0ded3
shankar7979 <shankar7979@gmail.com>
2023-10-26 12:04:03 +0530

    added

--- jwt spring boot -angular/spring boot2/spring boot 2  jwt jt/src/main/java/com/abc/jwt/api/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.abc.jwt.api.repository;
-
-import com.abc.jwt.api.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-    User findByUserName(String username);
-}
